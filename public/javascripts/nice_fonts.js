d585884547da9479b21d6510a6cb29b3a036b21a
omar gallo <omar@banus-dev.(none)>
2010-04-05 17:26:48 +0200

    whats'on layout

--- public/javascripts/nice_fonts.js
+++ public/javascripts/nice_fonts.js
@@ -8,15 +8,14 @@ jQuery.fn.nice_fonts = function(){
 	   		color: 'white'
 	  	 }
 	 });
-	Cufon.replace('#sub_navbar ul li', {
+	Cufon.replace('h1', {
 		   fontFamily: "Quicksand Bold",
 		   color: 'black',
-		   fontSize: "20px",
+		   fontSize: "24px",
 	  		hover: {
 	   		color: 'white'
 	  	 }
 	 });
-
 	 	Cufon.replace('h4', {
 		   fontFamily: "Quicksand Bold",
 		   color: 'black',
@@ -25,48 +24,4 @@ jQuery.fn.nice_fonts = function(){
 	   		color: 'white'
 	  	 }
 	 });
-	 	Cufon.replace('#travellers h1', {
-		   fontFamily: "Helvetica Neue LT Std",
-		   color: 'black',
-		   fontSize: "20px",
-	  		hover: {
-	   		color: 'white'
-	  	 }
-	 });	
-	 	Cufon.replace('#travellers h2', {
-		   fontFamily: "Helvetica Neue LT Std",
-		   color: 'black',
-		   fontSize: "18px",
-	  		hover: {
-	   		color: 'white'
-	  	 }
-	 });
-
-	 	Cufon.replace('#articles h3', {
-		   fontFamily: "Helvetica Neue LT Std",
-		   color: 'black',
-		   fontSize: "17px",
-	  		hover: {
-	   		color: 'white'
-	  	 }
-	 });
-
-	 	Cufon.replace('#classified h3', {
-		   fontFamily: "Helvetica Neue LT Std",
-		   color: 'black',
-		   fontSize: "16px",
-	  		hover: {
-	   		color: 'white'
-	  	 }
-	 });
-	 	Cufon.replace('#whatson h3', {
-		   fontFamily: "Helvetica Neue LT Std",
-		   color: 'black',
-		   fontSize: "15px",
-	  		hover: {
-	   		color: 'white'
-	  	 }
-	 });
-
-
 }
