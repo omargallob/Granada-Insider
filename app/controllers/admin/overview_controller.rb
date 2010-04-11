fc40b8f3294f453e137068e1fcc28ca821fef836
omar gallo <omar@banus-dev.(none)>
2010-04-11 13:18:04 +0200

    add aasm gem to .gems

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -1,5 +1,10 @@
 class Admin::OverviewController < Admin::BaseController
   def index
+		@pages = Page.find(:all)
+		@events = Event.find(:all)
+		@locations = Location.find(:all)
+		@users = User.find(:all)
+		@roles = Role.find(:all)
   end
 
 end
