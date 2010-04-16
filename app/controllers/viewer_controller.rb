c7fa81d15c99b318e4b089fa6a6bd6f7b8642b19
omar gallo <omar@banus-dev.(none)>
2010-04-16 18:53:10 +0200

    events looking sweet

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -2,7 +2,7 @@ class ViewerController < ApplicationController
   def show
 		@page = Page.find_by_name(params[:name])
 		if @page.name == "whatson"
-			@events = Event.find(:all, :order =>"start_at asc", :conditions => ["start_at > ?",Time.now], :include => "location")
+			@events = Event.find(:all, :order =>"start_at asc", :conditions => ["start_at > ?",Time.now], :include => "location").paginate :page => params[:page],:per_page => 6
 		end
 		if @page.name == "home"
 			@articles = Post.find_sub(2)
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,12 @@ class Event < ActiveRecord::Base
   has_event_calendar
   belongs_to :location
 	has_many :egalleries
-	include AASM
 
+  validates_length_of       :description,    :within => 140..170
+
+	include AASM
+	cattr_reader :per_page
+	@@per_page = 2
   aasm_column :state # defaults to aasm_state
 
   aasm_initial_state :open
