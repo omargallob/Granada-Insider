b33ae119f52fd95c88421c4e0344f3c2ba71547e
Omar Gallo <omargallob@gmail.com>
2010-05-10 22:24:50 +0200

    magazine sublevel css

--- app/controllers/issuu_controller.rb
+++ app/controllers/issuu_controller.rb
@@ -1,6 +1,8 @@
 class IssuuController < ApplicationController
   def index
-    @page = Page.find_by_name("magazine")    
+    @page = Page.find_by_name("magazine")
+    @news = Post.find_sub(3).paginate :page => 1,:per_page => 1
+		    
   end
 
   def show
